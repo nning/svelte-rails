56180209657e94bcc699e05cf2ab2b08b48ff2f6
henning mueller <mail@nning.io>
2020-07-30 23:28:18 +0200

    Add Rails system test to test SSR from view and update by hydration

--- test.sh
+++ test.sh
@@ -23,8 +23,26 @@ cat <<EOF > app/views/greetings/show.html.erb
 <%= svelte_component :Hello, {name: 'Test'}, {prerender: true} %>
 EOF
 
-rails s -d
-
-xdg-open http://localhost:3000/greetings/show
+# rails s -d
+# xdg-open http://localhost:3000/greetings/show
 
 # kill `cat tmp/pids/server.pid`
+
+rails db:migrate
+
+cat <<EOF > test/system/features_test.rb
+require 'application_system_test_case'
+
+class FeaturesTest < ApplicationSystemTestCase
+  test 'SSR from view and update by hydration' do
+    visit greetings_show_url
+
+    assert_selector 'h1', text: 'Hello Test! ❤'
+    assert !find('code').text.nil?
+  end
+end
+EOF
+
+sed -i 's/driven_by :selenium.*$/driven_by :selenium, using: :headless_chrome/g' test/application_system_test_case.rb
+
+rails test:system
